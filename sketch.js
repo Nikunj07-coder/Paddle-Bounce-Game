8b20860a8b8b67a73b37d6f99aae0b180927a958
Nikunj07-coder <64625446+Nikunj07-coder@users.noreply.github.com>
2020-09-07 21:04:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var paddle,ball,edge1,edge2,edge3,gameState;
+function setup() {
+  createCanvas(800,400);
+  paddle=createSprite(395,395,200,10);
+  paddle.shapeColor="blue";
+  edge1=createSprite(1,395,10,1000);
+  edge1.shapeColor="black";
+  edge2=createSprite(1,5,10000,10);
+  edge2.shapeColor="black";
+  edge3=createSprite(795,1,10,1000);
+  edge3.shapeColor="black";
+  ball=createSprite(400,200,15,15);
+  ball.shapeColor="white";
+  gameState="PLAY"
+}
+
+function draw() {
+  background("black");
+  if(gameState==="PLAY"){
+    text("Press Space to Start",100,100);
+  }
+  if(keyDown("space")){
+    ball.velocityY=5;
+    ball.velocityX=5;
+    gameState="END"
+  }
+  ball.bounceOff(paddle);
+  ball.bounceOff(edge1);
+  ball.bounceOff(edge2);
+  ball.bounceOff(edge3);
+  paddle.x=mouseX;
+  if(ball.y>400){
+    ball.x=400;
+    ball.y=200;
+  }
+  drawSprites();
+}
